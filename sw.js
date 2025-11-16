ed0057d5d07e3a0dc39ce2f63bcb1fa6fb027f75
McBruno22 <stanica_costel@yahoo.com>
2025-11-16 19:53:34 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "chat-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/admin.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE).catch(() => {});
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  // pentru socket.io și websocket lăsăm rețeaua direct
+  if (url.pathname.startsWith("/socket.io")) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
